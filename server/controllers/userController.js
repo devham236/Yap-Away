4e0a642e34b13335ee213b9ad7c908ca7214f5cc
devham236 <90037750+devham236@users.noreply.github.com>
2023-09-25 12:42:51 +0200

    clean up

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,4 @@
 const UserModel = require("../models/UserModel");
-const ImageModel = require("../models/imageModel");
 const genToken = require("../utils/genToken");
 const jwt = require("jsonwebtoken");
 
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
-app.use(express.json());
+// app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
