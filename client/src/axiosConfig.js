2ded8cdf2dcf13637b44bda41ca2077e061431ed
devham236 <90037750+devham236@users.noreply.github.com>
2023-09-30 11:34:15 +0200

    clean up

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
 axios.defaults.baseURL = "https://chat-app-backend-virid.vercel.app/api/v1"
-// axios.defaults.baseURL = "http://localhost:3000/api/v1"
 const token = JSON.parse(sessionStorage.getItem("token"))
 
 if (token) {
