74f475c0af0e5c9aba041d8ff729f8ae2500c1b5
devham236 <90037750+devham236@users.noreply.github.com>
2023-10-04 14:59:36 +0200

    clean up

--- client/src/Tests/Navbar.test.jsx
+++ client/src/Tests/Navbar.test.jsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import { describe, expect, test } from "vitest"
 import Navbar from "./../Components/Navbar"
+import { ContextProvider } from "../Context/Context"
+import { BrowserRouter } from "react-router-dom"
 
 describe("Navbar", () => {
   test("Renders text", () => {
-    render(<Navbar />)
+    render(
+      <ContextProvider>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </ContextProvider>
+    )
     const textEl = screen.getByText("Chat")
     expect(textEl).toBeInTheDocument()
   })
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./setupTests.js"],
+    setupFiles: ["./src/setupTests.js"],
   },
 })
