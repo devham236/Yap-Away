2f9442e491cfef74993ee7429f73e5627d148d4c
devham236 <90037750+devham236@users.noreply.github.com>
2023-10-25 14:07:58 +0200

    updated gitignore

---
+++ client/src/Config/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+axios.defaults.baseURL = "https://chat-app-server-sigma.vercel.app/api/v1"
+
+const token = JSON.parse(sessionStorage.getItem("token"))
+
+if (token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+}
+
+export default axios
