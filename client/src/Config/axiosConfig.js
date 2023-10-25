caa56d903bca0bef01e5d714c68bf3590fde47f5
devham236 <90037750+devham236@users.noreply.github.com>
2023-10-25 15:59:52 +0200

    cors error

--- client/src/Config/axiosConfig.js
+++ client/src/Config/axiosConfig.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
 axios.defaults.baseURL = "https://chat-app-server-sigma.vercel.app/api/v1"
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
 
 const token = JSON.parse(sessionStorage.getItem("token"))
 
 if (token) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
-  axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
 }
 
 export default axios
