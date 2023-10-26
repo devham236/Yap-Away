90cb8479e37b5cd64bbf8f0b357f468d34d07faf
devham236 <90037750+devham236@users.noreply.github.com>
2023-10-26 09:40:50 +0200

    v

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.use("/api/v1/chat", chatRouter);
 app.use("/api/v1/user", userRouter);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
